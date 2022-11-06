d3162bdd54af3923dd85b2adda25074292ec523e
oliver8578 <351868078@qq.com>
2022-11-07 01:57:07 +0800

    c

---
+++ Python_First1.py
@@ -0,0 +1,13 @@
+
+a=1
+b=2
+print(a+b)
+
+import random
+ip1=random.randint(1,255)
+ip2=random.randint(1,255)
+ip3=random.randint(1,255)
+ip4=random.randint(1,255)
+random_ip=str(ip1)+'.'+str(ip2)+'.'+str(ip3)+'.'+str(ip4)
+print(random_ip)
+
