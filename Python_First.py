6185d0235924952d1e2f273d1345d0d4ab44abb9
oliver8578 <351868078@qq.com>
2022-11-07 01:42:41 +0800

    commit2

--- Python_First.py
+++ Python_First.py
@@ -1,3 +1,20 @@
+# 这是一个示例 Python 脚本。
+
+# 按 ⌃R 执行或将其替换为您的代码。
+# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
+
+
+# 按间距中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+
 a=1
 b=2
 print(a+b)
@@ -10,4 +27,6 @@ ip4=random.randint(1,255)
 random_ip=str(ip1)+'.'+str(ip2)+'.'+str(ip3)+'.'+str(ip4)
 print(random_ip)
 
-
+a=1
+b=2
+print(a+b)
